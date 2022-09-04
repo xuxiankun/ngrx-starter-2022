2bd59c392f8f608078d73993974f21f04de65e39
xuxiankun <xuxiankun@gmail.com>
2022-09-03 20:11:16 -0600

    Update elements.component.ts

--- projects/angular-ngrx-material-starter/src/app/features/examples/elements/elements.component.ts
+++ projects/angular-ngrx-material-starter/src/app/features/examples/elements/elements.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy, AfterViewInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/animations/route.animations';
-
 @Component({
   selector: 'anms-elements',
   templateUrl: './elements.component.html',
   styleUrls: ['./elements.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ElementsComponent implements OnInit {
+export class ElementsComponent implements OnInit, OnDestroy, AfterViewInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   count$ = new BehaviorSubject(0);
-
   constructor() {}
-
   ngOnInit() {}
-
+  ngOnDestroy() {
+	removeEventListener('mr-clean-counter', ((event: CustomEvent) => {}) as EventListener);
+  }
+  ngAfterViewInit() {
+	addEventListener('mr-clean-counter', ((event: CustomEvent) => {
+  	this.setCounter(event);
+	}) as EventListener );
+  }
+  setCounter(event: CustomEvent) {
+	this.count$.next(event?.detail?.counter);
+}
   increment() {
-    const current = this.count$.getValue() + 1;
-    this.count$.next(current);
+	const current = this.count$.getValue() + 1;
+	this.count$.next(current);
   }
 }
